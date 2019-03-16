b4e14a8df015313aef40b269ef1dad818619c1ce
Erik Hetzner <egh@e6h.org>
2019-03-16 11:32:39 -0700

    Retry with TSLv1 if connection errors with unsupported protocol

--- ofxclient/client.py
+++ ofxclient/client.py
@@ -154,7 +154,15 @@ class Client:
         logging.debug('posting data to %s' % i.url)
         garbage, path = splittype(i.url)
         host, selector = splithost(path)
-        h = HTTPSConnection(host, timeout=60)
+        try:
+            h = HTTPSConnection(host, timeout=60)
+            h.connect()
+        except ssl.SSLError as ex:
+            if (ex.reason == "UNSUPPORTED_PROTOCOL"):
+                h = HTTPSConnection(host, timeout=60, context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
+                h.connect()
+            else:
+                raise
         # Discover requires a particular ordering of headers, so send the
         # request step by step.
         h.putrequest('POST', selector, skip_host=True,
